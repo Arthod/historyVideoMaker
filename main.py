38316d404ae55d8aec60bf06723cdc1a08dd002e
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2022-09-09 21:48:22 +0200

    zoom

--- main.py
+++ main.py
@@ -4,20 +4,51 @@ import numpy as np
 width, height = 1920, 1080
 fps = 60
 
-def crop_image(img: np.array, x1: int, y1: int, width: int, height: int) -> np.array:
-    return img[y1:y1 + height:1, x1:x1 + width:1]
+def lerps_linear(val_start: float, val_stop: float, count: int) -> list[float]:
+    b = val_start
+    a = (val_stop - b) / (count - 1)
+    
+    return [a * i + b for i in range(count)]
 
-img = cv2.imread("rivers.png").astype("uint8")
+def crop_image(img: np.array, x: int, y: int, width: int, height: int) -> np.array:
+    return img[y:y + height, x:x + width]
+
+def center_on_image(img: np.array, x: int, y: int, zoom: float):
+    x_margin = int(width/2 * zoom)
+    x = max(min(x, img.shape[1] - x_margin), x_margin)
+
+    y_margin = int(height/2 * zoom)
+    y = max(min(y, img.shape[0] - y_margin), y_margin)
+
+    w = int(width * zoom)
+    h = int(height * zoom)
+
+    print(x - w//2, y - h//2, w, h)
+
+    img = crop_image(img, x - w//2, y - h//2, w, h)
+    print(img.shape)
+    return img
+
+if __name__ == "__main__":
+
+    img = cv2.imread("images/map/rivers.png").astype("uint8")
 
 
-video = cv2.VideoWriter('video.mp4', cv2.VideoWriter_fourcc(*"mp4v"), fps, (width, height))
-#video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*"MJPG"), fps, (width, height))
+    video = cv2.VideoWriter('video.mp4', cv2.VideoWriter_fourcc(*"mp4v"), fps, (width, height))
+    #video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*"MJPG"), fps, (width, height))
 
-for i in range(10 * fps):
-    img2 = crop_image(img, i, 0, width, height)
+    zoom_stages = lerps_linear(1, 2, 5 * fps)
+    for i in range(5 * fps):
+        img_final = center_on_image(img, i * 7 + 300, i * 7 + 300, zoom_stages[i])
 
-    video.write(img2)
+        shape = img_final.shape
+        if (shape[0] != height or shape[1] != width):
+            img_final = cv2.resize(img_final, (width, height))#, interpolation=cv2.INTER_LINEAR)
+            print(f"Resizing: {img_final.shape}")
 
-video.release()
 
-print("Rendering complete")
+        video.write(img_final)
+
+    video.release()
+
+    print("Rendering complete")
